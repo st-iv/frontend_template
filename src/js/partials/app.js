6c426038b579217d3bc8ad88c9a8fc4259910ed3
Ivanov Stepan <stepan.ivanov93@gmail.com>
2017-10-10 10:49:22 +0300

    add slider

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -5,35 +5,5 @@ $('#slider').slick({
   speed: 300,
   slidesToShow: 1,
   // appendDots: $('#slider .dots'),
-  adaptiveHeight: true,
-
-  // customPaging : function(slider, i) {
-  // var title = $(slider.$slides[i]).data('title');
-  // var thumb = $(slider.$slides[i]).data('thumb');
-            
-  // return '<div class="dot-trumb"><a class="pager__item"><img class="dot__img" src="'+thumb+'"><span class="dot__desc">'
-  // + title +'</span></a></div>';
-  
-  // },
-
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        prevArrow: '<div class="prev"></div>',
-        nextArrow: '<div class="next"></div>',
-        arrows: true,
-        centerPadding: '40px',
-        slidesToShow: 1,
-        customPaging : function(slider, i) {
-          var title = $(slider.$slides[i]).data('title');
-          var thumb = $(slider.$slides[i]).data('thumb');
-            
-  return '<div class="dot-trumb"><a class="pager__item"><img class="dot__img" src="'+thumb+'"><span class="dot__desc">'
-  + title +'</span></a></div>';
-  
-}, 
-      }
-    },
-  ]
+  adaptiveHeight: true
 });
